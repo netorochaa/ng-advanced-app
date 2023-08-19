cb4a08a3f4c33f2e1f2f926d3cacbdbd9424242a
netorochaa <jrr.neto@hotmail.com>
2023-08-19 17:42:47 -0300

    create employees component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employees',  loadComponent: () => import('./pages/employees/employees.component').then(c => c.EmployeesComponent) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/employees/employees.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-employees',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './employees.component.html',
+})
+export class EmployeesComponent {
+
+}
