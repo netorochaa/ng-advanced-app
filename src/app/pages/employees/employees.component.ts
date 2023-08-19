2a0f14992597b4319fa312b74e6b27265b498a6a
netorochaa <jrr.neto@hotmail.com>
2023-08-19 19:25:37 -0300

    load employees

--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { EmployeeService } from './service/employee-service';
 import { HttpClientModule } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { IEmployee } from './interfaces/Employee';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, TableModule, AvatarModule],
   providers: [EmployeeService],
   standalone: true,
 })
 export class EmployeesComponent implements OnInit {
   constructor(readonly employeeService: EmployeeService) {}
 
-  employees$!: Observable<any>;
+  employees$!: Observable<IEmployee[]>;
 
   ngOnInit(): void {
     this.employees$ = this.employeeService.get();
--- src/app/pages/employees/service/employee-service.ts
+++ src/app/pages/employees/service/employee-service.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IEmployee } from '../interfaces/Employee';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class EmployeeService {
 
   constructor(readonly http: HttpClient) { }
 
-  get(): Observable<any[]> {
-    return this.http.get<any[]>(`https://random-data-api.com/api/v2/users?size=10`);
+  get(): Observable<IEmployee[]> {
+    return this.http.get<IEmployee[]>(`https://random-data-api.com/api/v2/users?size=10`);
   }
 }
