887b25e737bcd9c71280d5deb328cf973452bf29
netorochaa <jrr.neto@hotmail.com>
2023-08-20 14:32:10 -0300

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'employees',  loadComponent: () => import('./pages/employees/employees.component').then(c => c.EmployeesComponent) }
+  { path: 'employees',  loadComponent: () => import('./pages/employees/employees.component').then(c => c.EmployeesComponent) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
 ];
 
 @NgModule({
